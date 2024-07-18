0deffba0840a470c566589dc503f644842f0e570
fajar <fajar.anwarimaulana99@gmail.com>
2024-07-18 12:55:54 +0700

    Fix(main): change value in env

--- serverapp/src/main.ts
+++ serverapp/src/main.ts
@@ -2,12 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import * as dotenv from 'dotenv';
+import * as process from 'node:process';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieParser())
-  await app.listen(3000);
+  app.use(cookieParser());
+  await app.listen(process.env.PORT);
 }
 bootstrap();
